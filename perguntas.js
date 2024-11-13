620a538f333a1b790664a2cc0a1505ec7a6958d1
nicollymendonca <164960912+nicollymendonca@users.noreply.github.com>
2024-11-13 20:37:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital dO Brasil?',
+    'A capital é Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "água" em Inglês?',
+    'Água em ingles é water (uarer)'
+)
